aeb8a58ddc9c73ffd28ed44a7e79815116c5cdbe
sahan <fs.sahan09@gmail.com>
2025-12-16 17:30:59 +0300

    backend .net api,swaggerUI,EntityClass,ConnectionString,DbContext,Migration

---
+++ e-commerce-Backend/commerce.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products=>Set<Product>();
+    }
+}
---
+++ e-commerce-Backend/commerce.API/Program.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("DefaultConnection");
+
+    options.UseSqlite(connectionString);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "commerce.API v1");
+    });
+}
+
+app.MapControllers();
+app.Run();
