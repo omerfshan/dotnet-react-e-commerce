7263174e4fb011f123fe54619dff20bd3427cd89
sahan <101819553+omerfshan@users.noreply.github.com>
2026-01-05 18:46:29 +0300

    enhance CartController to manage customer cart creation and item removal; add cookie handling for customer ID

--- e-commerce-Backend/commerce.API/Controllers/CartController.cs
+++ e-commerce-Backend/commerce.API/Controllers/CartController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 namespace API.Controllers;
 
@@ -20,24 +21,63 @@ public class CartController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<Cart>> GetCart()
     {
-        var cart = await _context.Carts
-            .Include(c => c.CartItems)
-            .ThenInclude(ci => ci.Product)
-            .FirstOrDefaultAsync(c => c.CustomerId == Request.Cookies["customerId"]);
-
-        if (cart is null) return NotFound();
+        var cart = await GetOrCreate();
 
         return cart;
     }
-    // public async Task<ActionResult<Cart>> GetOrCreate()
-    //     {
-    //             var cart=await _context.Carts.Include(c => c.CartItems).
-    //             ThenInclude(ci => ci.Product).
-    //             Where(c => c.CustomerId == Request.Cookies["CustomerId"]).
-    //             FirstOrDefaultAsync();
-    //             if (cart == null)
-    //             {
-    //                     var customerId =Guid.NewGuid().ToString();
-    //             }
-    //     }
+    [HttpPost]
+    public async Task<ActionResult> AddItemToCart(int productId,int quantity)
+    {
+        var cart =await GetOrCreate();
+        var product=await _context.Products.FirstOrDefaultAsync(i=>i.Id == productId);
+        if(product==null)
+        return NotFound();
+        cart.AddItem(productId,quantity);
+        var result=await _context.SaveChangesAsync()>0;
+        if (result)
+        {
+            return CreatedAtAction(nameof(GetCart),cart);
+        }
+        return BadRequest(new ProblemDetails { Title="The product can not be added to cart"});
+    }
+    // DELETE: api/cart/{id}
+[HttpDelete]
+public async Task<IActionResult> DeleteFromCart(int ProductId,int quantity)
+{
+    var cart = await GetOrCreate();
+
+    cart.RemoveItem(ProductId,quantity);
+
+    _context.Carts.Remove(cart);
+    var result= await _context.SaveChangesAsync()>0;
+    if(result)
+    return Ok();
+    return  BadRequest(new ProblemDetails { Title="problem removing item "}) ;
+}
+
+    private async Task<Cart> GetOrCreate()
+    {
+         var cart = await _context.Carts
+            .Include(c => c.CartItems)
+            .ThenInclude(ci => ci.Product)
+            .FirstOrDefaultAsync(c => c.CustomerId == Request.Cookies["customerId"]);
+        if(cart is null)
+        {
+            var customerId=Guid.NewGuid().ToString();
+            var CookiesOptions=new CookieOptions
+            {
+                Expires=DateTime.Now.AddMonths(1),
+                IsEssential=true,
+            };
+            Response.Cookies.Append("customerId",customerId,CookiesOptions);
+            cart =new Cart{CustomerId=customerId};
+            _context.Carts.Add(cart);
+            await _context.SaveChangesAsync();
+        }
+        return cart;
+     
+        
+
+    
+    }
 }
